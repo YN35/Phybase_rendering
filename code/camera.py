acd6a385d8d16af19eb0e3218f6387167c6b67d6
YN35 <yono003500@gmail.com>
2022-05-19 10:39:49 +0900

    make classes

---
+++ code/camera.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+class Camera():
+    
+    def __init__(self) -> None:
+        pass
+    
+    def rendering(self):
+    
+    def get_pixel_color(self,x):
+        
+        
+    
+    
+    
---
+++ code/lighting.py
@@ -0,0 +1,9 @@
+
+#照明に関する情報を返す
+
+class Lighting():
+    
+    def __init__(self) -> None:
+        pass
+    
+    def 
